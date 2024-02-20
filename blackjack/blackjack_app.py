0d0846ac99788dcd67210fd2061fa2bd0c43ab92
Your Name <ryanzmudka@gmail.com>
2024-02-20 16:00:44 -0500

    add blackjack

---
+++ blackjack/blackjack_app.py
@@ -0,0 +1,11 @@
+"""
+Create a Blackjack Game
+
+- Display the user's cards (add the values to help user)
+- Display Computer's first card
+- Ask for y to hit and n to stand
+ - If no then display final value of hand, have computer draw a card and then the Computer's final hand
+ - if yes then give player another card, display new value, determine if blackjack or bust, ask to hit again or stand
+"""
+
+
