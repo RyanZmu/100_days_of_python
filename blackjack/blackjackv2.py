eaa8938ed7fa66d303d8c6df70e87f98de04037e
Your Name <ryanzmudka@gmail.com>
2024-03-18 14:41:39 -0400

    add bets

--- blackjack/blackjackv2.py
+++ blackjack/blackjackv2.py
@@ -1,28 +1,58 @@
 import random
 import modules.deck as deck
 import utils.format_text as formatter
+# import turtle as t
+# import os
 
-# Make the game have a GUI
-# from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
+# Build graphics with turtle
 
-# import sys
+# color('blue')
+# forward(100)
+# color('red')
+# left(120)
+# color('green')
+# forward(100)
+# color('red')
+# right(120)
+# color('blue')
+# backward(100)
+# color('red')
+# home()
 
 
-# class MainWindow(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
+# t.color('red')
+# t.fillcolor('yellow')
 
-#         self.setWindowTitle("Hello World")
+# t.begin_fill()
 
-#         button = QPushButton("My simple app.")
-#         button.pressed.connect(self.close)
+# while True:
+#     t.forward(200)
+#     t.left(170)
+#     if abs(t.pos()) < 1:
+#         break
 
-#         self.setCentralWidget(button)
-#         self.show()
+# t.end_fill()
 
-# app = QApplication(sys.argv)
-# w = MainWindow()
-# app.exec()
+# t = t.Turtle(
+#    shape="turtle"
+# )
+
+# for i in range(100):
+#     steps = int(random.random() * 100)
+#     angle = int(random.random() * 360)
+#     t.right(angle)
+#     t.fd(steps)
+
+# t.screen.title('Object-oriented turtle demo')
+# t.screen.bgcolor("orange")
+# t.screen.title("Blackjack!")
+# playername = t.screen.textinput("Name", "Name of Player")
+
+# t.screen.onclick(t.goto(float(random.randint(100,540)), float(random.randint(100,540))))
+# t.home()
+# t.screen.register_shape('card', )
+
+# t.screen.mainloop()
 
 """
 Create a Blackjack Game
@@ -53,6 +83,19 @@ Psuedo:
  - Output who got the Blackjack 
 - 3rd: Stretch - Betting and Chip Storage
 """
+
+print("""
+          _____
+         |A .  | _____
+         | /.\ ||A ^  | _____
+Welcome! |(_._)|| / \ ||A _  | _____
+         |  |  || \ / || ( ) ||A_ _ |
+         |____V||  .  ||(_'_)||( v )|
+                |____V||  |  || \ / |
+                       |____V||  .  |
+                              |____V|
+
+""")
 
 DECK_OF_CARDS = deck.DECK_OF_CARDS
 
@@ -103,14 +146,18 @@ def blackjack_game():
         # Init card draws for player
     if not end_game:
 
+        player_bet: int = 0
+
         if first_draw:
             draw_card(hand=player_hand,name='player')
             draw_card(hand=dealer_hand,name='dealer')
             first_draw = False
 
+            player_bet = int(input(f'You have {player_hand['chips']} how many chips do you bet?'))
+            print(f"You have bet {player_bet} chips and have {player_hand['chips'] - player_bet} chips left\n")
+
         player_prompt: str = input(f"Your hand: {player_hand['cards_in_hand']} for a total of {player_hand['value']}\n"\
                                    f"Dealer hand: {dealer_hand['cards_in_hand']} for a total of {dealer_hand['value']}\n"\
-                                   f"You have a total of {player_hand['chips']} chips\n"\
                                    f"Do you want to Hit or Stand?\n")
     
         if player_prompt.lower() == 'hit':
@@ -121,12 +168,15 @@ def blackjack_game():
             draw_card(hand=dealer_hand,name='dealer')
         elif dealer_hand['value'] > 17 and player_prompt.lower() == 'stand' and player_hand['value'] > dealer_hand['value']:
             end_game = True
+            player_hand['chips'] += player_bet * 2
             print('PLAYER WINS!')
         elif player_prompt.lower() == 'stand' and dealer_hand['value'] < player_hand['value']:
             end_game = True
+            player_hand['chips'] += player_bet * 2
             print("PLAYER WINS!")
         elif dealer_hand['value'] == player_hand['value']:
            end_game= True
+           player_hand['chips'] += player_bet
            print('DEALER PUSH!')
     else:
        return
