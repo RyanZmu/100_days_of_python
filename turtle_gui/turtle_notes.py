7de2549db8fe67bf7f99eb3f2e9a084610541b34
Your Name <ryanzmudka@gmail.com>
2024-04-09 17:09:27 -0400

    add turtle gui

---
+++ turtle_gui/turtle_notes.py
@@ -0,0 +1,27 @@
+from turtle import Screen, Turtle
+
+timmy_turtle = Turtle()
+timmy_turtle.shape("turtle")
+timmy_turtle.color("green")
+
+# Challenge 1: Draw a Square
+timmy_turtle.forward(100)
+timmy_turtle.right(90)
+timmy_turtle.forward(100)
+timmy_turtle.right(90)
+timmy_turtle.forward(100)
+timmy_turtle.right(90)
+timmy_turtle.forward(100)
+timmy_turtle.right(90)
+
+
+screen = Screen()
+screen.exitonclick()
+
+
+# Importing Modules
+# Alias
+import random as r
+print(r.randint(1,20))
+
+
