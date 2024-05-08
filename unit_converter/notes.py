3dc62acd97561548569f84ba53c22f146d8cfb59
Your Name <ryanzmudka@gmail.com>
2024-05-08 16:25:16 -0400

    kwargs and args

--- unit_converter/notes.py
+++ unit_converter/notes.py
@@ -1,36 +1,29 @@
 """
 Working with Tkinter GUI
 """
-import tkinter
+# Pull all tkinter and no longer needs module.property syntax
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program!")
 window.minsize(width=900, height=700)
 
 
 # Label - first create and then specify the layout on screen
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 
 # pack will automatically center
 my_label.pack()
 
+# Change text property with keyword or config
+my_label["text"] ="New Text"
+my_label.config(text="New Text")
 
-"""
-Default and keywords
-**kw and **args lets you have default arguments for functions that dont need specifying every function call
+# Button
+button = Button(text="Click")
+button.pack()
 
-When you see a argument that looks like this: arg=... it means there is a default value for that argument
-Can set what arguments are required and what ones are optional and are defaulted
 
-This will allow you to set default arguments and require arguments with each function
-"""
-# *args
 
-# Create a function that takes unlimited arguments - use *args and loop the arguments, now any amount of arguments can be used
-def add(*args):
-    for n in args:
-        print(n)
-
-add(1,10,2)
 # mainloop keeps window on screen - alwasy at end of program
 window.mainloop()
--- unit_converter/playground.py
+++ unit_converter/playground.py
@@ -1,5 +1,13 @@
 """
 *args *kwargs playground
+
+Default and keywords
+**kw and **args lets you have default arguments for functions that dont need specifying every function call
+
+When you see a argument that looks like this: arg=... it means there is a default value for that argument
+Can set what arguments are required and what ones are optional and are defaulted
+
+This will allow you to set default arguments and require arguments with each function
 """
 # Make function to add any amount of numbers
 # **args puts the arguments into a tuple - can access by index
@@ -31,18 +39,17 @@ calculate(2,add=3, multiply=5)
 
 #  Can use kwargs["name_of_argument"] to create optional args for the class
 class Car:
-    def __init__(self, **kwargs):
+    def __init__(self, owner, **kwargs):
         self.make = kwargs.get("make")
-        self.model = kwargs.get("mode")
+        self.model = kwargs.get("model")
         self.color = kwargs.get("color")
+        self.owner = owner
 
 # Note there are only **kwargs when hovering over the var
 # Will fail if a kwarg is missing
 # Using .get above will avoid this fail and just return None
 # This allows us to make optional keyword arguments
-my_car = Car(make="Nissan")
-print(my_car.model)
+my_car = Car(make="Nissan", owner="Ryan", model="GT-R")
+print(my_car.owner)
 print(my_car.make)
-
-
-
+print(my_car.model)
