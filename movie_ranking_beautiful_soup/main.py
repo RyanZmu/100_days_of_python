1a597ea31cf0a925b814314aed7eca31489f817b
Your Name <ryanzmudka@gmail.com>
2024-08-19 17:14:18 -0400

    completed beautiful soup project

--- movie_ranking_beautiful_soup/main.py
+++ movie_ranking_beautiful_soup/main.py
@@ -1,5 +1,25 @@
 """
-Movie Rankings
-"""
+Movie Rankings:
 
+- Use Empire's Top 100 Movies of All Time list
+- Scape the list and place it into a text file
+ - Number and Title listed
+"""
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(url="https://www.empireonline.com/movies/features/best-movies-2/")
+
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup.prettify())
+
+get_titles = soup.findAll(name="h3")
+titles_list = [title.string for title in get_titles]
+
+
+# Create text file
+with open(file="./movie_ranking_beautiful_soup/movies.txt", mode="w") as movie_file:
+    # [::-1] will reverse a list
+    for title in titles_list[::-1]:
+        movie_file.write(f"{title}\n")
 
--- movie_ranking_beautiful_soup/notes.py
+++ movie_ranking_beautiful_soup/notes.py
@@ -1,19 +1,65 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open(file="./movie_ranking_beautiful_soup/website.html") as website:
-    contents = website.read()
+response = requests.get(url="https://news.ycombinator.com/news")
+# print(response.text
 
-soup = BeautifulSoup(contents, features="html.parser")
+# Get titles and links for each article
+soup = BeautifulSoup(response.text, features="html.parser")
+# print(soup.prettify())
 
-print(soup.prettify())
-# print(soup.title)
-# print(soup.title.name)
-# print(soup.title.string)
+article_tag = soup.select(selector=".titleline a")
+article_text = [article.getText() for article in article_tag]
+article_links = [article.get("href") for article in article_tag]
 
-all_anchors = soup.findAll(name="a")
+# Output all articles and links
+# print(article_text)
+# print(article_links)
 
-for tag in all_anchors:
-    print(tag.getText())
+# Output all up votes - runs getText on each item
+article_upvote = [int(score.getText().split()[0]) for score in soup.findAll(name="span", class_="score")]
+# print(article_upvote)
+
+# Find highest upvoted article
+highest_value: int = max(article_upvote)
+highest_value_index: int = article_upvote.index(highest_value)
+
+highest_upvoted_article = article_text[highest_value_index]
+highest_upvoted_link = article_links[highest_value_index]
+
+print(highest_upvoted_article)
+print(highest_upvoted_link)
 
 
-
+# Messing with Beautiful Soup locally
+# with open(file="./movie_ranking_beautiful_soup/website.html") as website:
+#     contents = website.read()
+#
+#
+# # Print different tags and output nicely
+# print(soup.prettify())
+# # print(soup.title)
+# # print(soup.title.name)
+# # print(soup.title.string)
+#
+# # Get all Anchor links
+# all_anchors = soup.findAll(name="a")
+# for tag in all_anchors:
+#     print(tag.get("href"))
+#
+# # Get specific heading with id=random
+# heading = soup.find(name="h1", id="random")
+# print(heading)
+#
+# # When looking for class attribute, add an _
+# paragraph = soup.find(name="p", class_="random")
+# print(paragraph)
+# print(paragraph.get("class"))
+#
+# # Getting nested elements
+# # Can use CSS selectors to get elements with soup
+# company_url = soup.select_one(selector="p em a")
+# print(company_url)
+#
+# all_headings = soup.select(".random")
+# print(all_headings)
